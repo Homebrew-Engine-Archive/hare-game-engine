47eb04ca514adcccd257d031f4bd3f81e238f16b
littlesome@live.cn <littlesome@live.cn@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-03-21 06:59:15 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@37 67360d68-41fe-4630-88ca-a5c15c9c3197

---
+++ editor/MIMEHandlerPlugin.cpp
@@ -0,0 +1,26 @@
+//***************************************************************
+//  File:    MIMEHandlerPlugin.h
+//  Data:    01/27/2009
+//  Author:  littlesome (littlesome@live.cn)
+//-------------------------------------------------------------
+//  
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+// 
+//***************************************************************
+#include "PCH.h"
+#include "MIMEHandlerPlugin.h"
+
+namespace hare_editor
+{
+    MIMEHandlerPlugin::MIMEHandlerPlugin()
+    {
+        type = EPT_MIMEHandler;
+    }
+
+    MIMEHandlerPlugin::~MIMEHandlerPlugin()
+    {
+    }
+}
---
+++ editor/wxHareCanvas/src/harecanvas.cpp
@@ -0,0 +1,52 @@
+//***************************************************************
+//  File:    harecanvas.cpp
+//  Data:    03/20/2009
+//  Author:  littlesome (littlesome@live.cn)
+//-------------------------------------------------------------
+//  
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+// 
+//***************************************************************
+#include "wx/harecanvas.h"
+
+const wxChar* wxHareCanvasNameStr = wxT("HareCanvas");
+
+wxHareCanvas::wxHareCanvas(wxWindow *parent, wxWindowID id, bool hasZBuffer, const wxPoint& pos,
+        const wxSize& size, long style, const wxString& name) : wxWindow()
+{
+    renderWindow = NULL;
+
+    Create(parent, id, pos, size, style, name);
+
+    WindowParams params;
+    params.hwnd = (u32)GetHWND();
+    params.bFullScreen = false;
+    params.width = size.GetWidth() > 0 ? size.GetWidth() : 800;
+    params.height = size.GetHeight() > 0 ? size.GetHeight() : 600;
+    params.bZbuffer = hasZBuffer;
+    params.title = name.ToUTF8().data();
+
+    renderWindow = getHareApp()->createRenderWindow(params);
+    renderWindow->destoryWindow();
+}
+
+wxHareCanvas::~wxHareCanvas()
+{
+    if (renderWindow)
+    {
+        //renderWindow->setSceneManager(0);
+        
+    }
+    renderWindow = NULL;
+}
+
+bool wxHareCanvas::Create(wxWindow *parent, wxWindowID id, const wxPoint& pos, 
+        const wxSize& size, long style, const wxString& name)
+{
+    wxCHECK_MSG(parent, false, wxT("can't create wxWindow without parent"));
+
+    return wxWindow::Create(parent, id, pos, size, style, name);
+}

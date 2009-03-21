b561b34e76a4331c1f72cb56d57786733ad37026
littlesome@live.cn <littlesome@live.cn@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-03-21 06:59:34 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@38 67360d68-41fe-4630-88ca-a5c15c9c3197

---
+++ include/editor/MIMEHandlerPlugin.h
@@ -0,0 +1,44 @@
+//***************************************************************
+//  File:    MIMEHandlerPlugin.h
+//  Data:    01/27/2009
+//  Author:  littlesome (littlesome@live.cn)
+//-------------------------------------------------------------
+//  
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+// 
+//***************************************************************
+#ifndef MIMEHANDLERPLUGIN_H
+#define MIMEHANDLERPLUGIN_H
+
+#include "EditorPrerequisites.h"
+#include "EditorPlugin.h"
+
+namespace hare_editor
+{
+    class EDITOR_API MIMEHandlerPlugin : public EditorPlugin
+    {
+    public:
+        MIMEHandlerPlugin();
+        virtual ~MIMEHandlerPlugin();
+
+    public:
+        virtual bool canHandle(const wxString& filename) const = 0;
+
+        virtual bool openFile(const wxString& filename) = 0;
+
+    private:
+        virtual bool buildMenuBar(wxMenuBar* menuBar)
+        {
+            return false;
+        }
+        virtual bool buildToolBar(wxAuiToolBar* toolBar)
+        {
+            return false;
+        }
+    };
+}
+
+#endif
---
+++ include/editor/wxHareCanvas/include/wx/harecanvas.h
@@ -0,0 +1,55 @@
+//***************************************************************
+//  File:    harecanvas.h
+//  Data:    03/20/2009
+//  Author:  littlesome (littlesome@live.cn)
+//-------------------------------------------------------------
+//  
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+// 
+//***************************************************************
+#ifndef HARECANVAS_H
+#define HARECANVAS_H
+
+#include <wx/window.h>
+#include <wx/string.h>
+
+#include "graphics/Graphics.h"
+using namespace hare_graphics;
+
+#ifdef WXMAKINGDLL_HARE_CANVAS
+#    define WXDLLIMPEXP_HARE_CANVAS WXEXPORT
+#elif defined(WXUSINGDLL_HARE_CANVAS)
+#    define WXDLLIMPEXP_HARE_CANVAS WXIMPORT
+#else
+#    define WXDLLIMPEXP_HARE_CANVAS
+#endif
+
+extern WXDLLIMPEXP_HARE_CANVAS const wxChar* wxHareCanvasNameStr;
+
+class WXDLLIMPEXP_HARE_CANVAS wxHareCanvas : public wxWindow
+{
+public:
+    wxHareCanvas(wxWindow *parent, wxWindowID id = wxID_ANY, bool hasZBuffer = true,
+        const wxPoint& pos = wxDefaultPosition,
+        const wxSize& size = wxDefaultSize, long style = 0,
+        const wxString& name = wxHareCanvasNameStr);
+
+    virtual ~wxHareCanvas();
+
+    bool Create(wxWindow *parent, wxWindowID id,
+        const wxPoint& pos, const wxSize& size,
+        long style, const wxString& name);
+
+    RenderWindow* getRenderWindow() const
+    {
+        return renderWindow;
+    }
+
+protected:
+    RenderWindow* renderWindow;
+};
+
+#endif

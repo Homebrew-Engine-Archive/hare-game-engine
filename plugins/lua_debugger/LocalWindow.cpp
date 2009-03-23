49b10010dfdba102d6b2232c408dd14036c2fd26
littlesome@live.cn <littlesome@live.cn@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-03-23 10:29:37 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@60 67360d68-41fe-4630-88ca-a5c15c9c3197

---
+++ plugins/lua_debugger/LocalWindow.cpp
@@ -0,0 +1,125 @@
+#include "PCH.h"
+#include "LocalWindow.h"
+#include "LuaDebugger.h"
+#include <wx/dnd.h>
+#include <wx/treelistctrl.h>
+
+static const wxChar* LUA_TYPE_NAME[] = {
+    wxT("nil"), wxT("boolean"), wxT("lightuserdata"), wxT("number"), wxT("string"),
+        wxT("table"), wxT("function"), wxT("userdata"), wxT("thread")
+};
+
+BEGIN_EVENT_TABLE(LuaLocalWindow, wxPanel)
+EVT_TREE_ITEM_EXPANDED(wxID_ANY, onTreeItemExpanded)
+END_EVENT_TABLE()
+
+LuaLocalWindow::LuaLocalWindow(wxWindow* parent, LuaDebugger* dbg)
+: wxPanel(parent), debugger(dbg)
+{
+    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+
+    treeList = new wxTreeListCtrl(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 
+        wxTR_HIDE_ROOT | wxTR_EDIT_LABELS | wxTR_FULL_ROW_HIGHLIGHT | 
+        wxTR_ROW_LINES | wxTR_COLUMN_LINES | wxTR_HAS_BUTTONS);
+    sizer->Add(treeList, 1, wxALIGN_LEFT | wxALIGN_TOP | wxEXPAND, 0);
+    SetSizer(sizer);
+    Layout();
+    sizer->Fit(this);
+
+    treeList->AddColumn(_("Name"));
+    treeList->AddColumn(_("Value"), 200);
+    treeList->AddColumn(_("Type"));
+
+    root = treeList->AddRoot(wxEmptyString);
+}
+
+LuaLocalWindow::~LuaLocalWindow()
+{
+}
+
+void LuaLocalWindow::onTreeItemExpanded(wxTreeEvent& event)
+{
+    wxTreeItemId id = event.GetItem();
+
+    if (id.IsOk())
+    {
+        wxString tableName = treeList->GetItemText(id, 0);
+
+        debugger->enumerateTable(debugger->getCurrStackLevel(), tableName.ToUTF8().data());
+    }
+    else
+        event.Veto();
+}
+
+void LuaLocalWindow::updateTableData(const String& tableName, LuaDebugData* debugData)
+{
+    if (debugData && debugData->items.size() > 0)
+    {
+        wxTreeItemIdValue cookie;
+        wxTreeItemId child = treeList->GetFirstChild(root, cookie);
+        while (child.IsOk())
+        {
+            wxString text = treeList->GetItemText(child, 0);
+
+            if (text == wxString::FromUTF8(tableName.c_str()))
+            {
+                treeList->DeleteChildren(child);
+
+                for (size_t i = 0; i < debugData->items.size(); ++i)
+                {
+                    wxString key = wxString::FromUTF8(debugData->items[i]->itemKey.c_str());
+                    wxString value = wxString::FromUTF8(debugData->items[i]->itemValue.c_str());
+                    wxString type = LUA_TYPE_NAME[debugData->items[i]->itemValueType];
+
+                    wxTreeItemId itemId = treeList->AppendItem(child, key);
+                    treeList->SetItemText(itemId, 1, value);
+                    treeList->SetItemText(itemId, 2, type);
+                }
+            }
+
+            child = treeList->GetNextChild(root, cookie);
+        }
+    }
+}
+
+void LuaLocalWindow::updateLocalData(LuaDebugData* debugData)
+{
+    wxString key;
+    wxString value;
+
+    treeList->DeleteChildren(root);
+
+    if (debugData && debugData->items.size() > 0)
+    {
+        for (size_t i = 0; i < debugData->items.size(); ++i)
+        {
+            key = wxString::FromUTF8(debugData->items[i]->itemKey.c_str());
+            value = wxString::FromUTF8(debugData->items[i]->itemValue.c_str());
+            key = wxString::FromUTF8(debugData->items[i]->itemKey.c_str());
+            wxTreeItemId child = treeList->AppendItem(root, key);
+            treeList->SetItemText(child, 1, value);
+            if (debugData->items[i]->itemValueType >= 0 &&
+                debugData->items[i]->itemValueType <= 8)
+            {
+                treeList->SetItemText(child, 2, LUA_TYPE_NAME[debugData->items[i]->itemValueType]);
+            }
+
+            if (debugData->items[i]->itemValueType == LUA_TTABLE)
+            {
+                treeList->AppendItem(child, wxEmptyString);
+            }
+        }
+    }
+}
+
+bool LuaLocalWindow::isReallyShown()
+{
+    wxWindow* win = this;
+    while (win && win->IsShown())
+    {
+        win = win->GetParent();
+        if (!win)
+            return true;
+    }
+    return false;
+}
---
+++ plugins/lua_debugger/LocalWindow.h
@@ -0,0 +1,42 @@
+//***************************************************************
+//  File:    LocalWindow.h
+//  Data:    03/23/2009
+//  Author:  littlesome (littlesome@live.cn)
+//-------------------------------------------------------------
+//  
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+// 
+//***************************************************************
+#ifndef LOCALWINDOW_H
+#define LOCALWINDOW_H
+
+#include <wx/xrc/xmlres.h>
+
+class LuaDebugger;
+class wxTreeListCtrl;
+
+class LuaLocalWindow : public wxPanel
+{
+public:
+    LuaLocalWindow(wxWindow* parent, LuaDebugger* dbg);
+    virtual ~LuaLocalWindow();
+
+    void updateLocalData(LuaDebugData* debugData);
+    void updateTableData(const String& tableName, LuaDebugData* debugData);
+    bool isReallyShown();
+
+protected:
+    LuaDebugger* debugger;
+    wxTreeListCtrl* treeList;
+    wxTreeItemId root;
+
+private:
+    void onTreeItemExpanded(wxTreeEvent& event);
+
+    DECLARE_EVENT_TABLE();
+};
+
+#endif

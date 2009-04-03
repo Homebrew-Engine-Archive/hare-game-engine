baedcbd6f1cc8d6a092dfb8191310ac3300c81c1
wanglei010407@hotmail.com <wanglei010407@hotmail.com@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-04-03 11:05:01 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@124 67360d68-41fe-4630-88ca-a5c15c9c3197

--- lib/scripts/sample/script.lua
+++ lib/scripts/sample/script.lua
@@ -1,6 +1,7 @@
 sprite1 = nil
 sprite2 = nil
 sceneListen = nil
+pp = nil
 
 lastTime = 0
 
@@ -11,16 +12,18 @@ function beginScene()
         lastTime = hare.getTime()
         print("fps:", hare.getTimer():getFPS())
     end
+    pp:frameMove()
 end
 
 function renderScene()
 	canvas = hare.getCanvas()
 	--canvas:drawImage(500, 600, shader)
-	--canvas:drawRect(50, 50, 100, 100, 0xffff0000)
-	--canvas:drawLine(0,0, 800, 600, 0xffffffff, 1)
-	--canvas:drawRect(50, 50, 600, 500, 0xff00ff00, 0.5)
+	canvas:drawRect(50, 50, 100, 100)
+	canvas:drawLine(0,0, 800, 600)
+	canvas:drawRect(50, 50, 600, 500)
  
-	canvas:drawText(50,50,'abc')
+	canvas:drawText(50,50,'abcsfsdfhh')
+	pp:render()
 end
 
 function endScene()
@@ -39,35 +42,52 @@ function init()
     s = hareApp:createSceneManager()
     w:setSceneManager(s)
     sprite1 = hare.SimpleSprite()
-    sprite1:loadFromImage("test.png")
-    s:addSprite(sprite1)
+    sprite1:loadFromImage("xueren.png")
+    --s:addSprite(sprite1)
     sprite2 = hare.SimpleSprite()
     sprite2:loadFromImage("yu.png")
-    --s:addSprite(sprite2)    
+    s:addSprite(sprite2)    
     listener = hare.LuaSceneListener()
     listener:setBeginSceneListenFunc(beginScene)
     listener:setRenderSceneListenFunc(renderScene)
     listener:setEndSceneListenFunc(endScene)
     s:setSceneListener(listener)
 
-    p = hare.WindowParams()
-    p.hwnd = 0
-    p.width = 200
-    p.height = 200
-    p.bFullScreen = false
-    p.bZbuffer = true
-    w = hareApp:createRenderWindow(p)
-    s = hareApp:createSceneManager()
-    w:setSceneManager(s)
-	s:addSprite(sprite2)
+    --p = hare.WindowParams()
+    --p.hwnd = 0
+    --p.width = 200
+    --p.height = 200
+    --p.bFullScreen = false
+    --p.bZbuffer = true
+    --w = hareApp:createRenderWindow(p)
+    --s = hareApp:createSceneManager()
+    --w:setSceneManager(s)
+	  --s:addSprite(sprite2)
+	  --s:setSceneListener(listener)
+	  
+	  --p = hare.WindowParams()
+    --p.hwnd = 0
+    --p.width = 200
+    --p.height = 200
+    --p.bFullScreen = false
+    --p.bZbuffer = false
+    --w = hareApp:createRenderWindow(p)
+    --s = hareApp:createSceneManager()
+    --w:setSceneManager(s)
+    --s:addSprite(sprite2)
+	  --s:setSceneListener(listener)  
 		
 	tm = hare.getTextManager() 
 	fnt = tm:createFont("comic.ttf", 20) 
 	canvas = hare.getCanvas()
 	canvas:setFont(fnt)
+	
+	pp = hare.Object_importObject('default.particle')
+	pp:fireAt(300,300)
 end
 
 function quit()
     sprite1 = nil
     sprite2 = nil
+    pp = nil
 end

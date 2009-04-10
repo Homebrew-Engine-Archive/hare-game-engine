c57a33bc7204f17102c15a37a321d063204c6b67
wanglei010407@hotmail.com <wanglei010407@hotmail.com@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-04-10 00:46:03 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@151 67360d68-41fe-4630-88ca-a5c15c9c3197

---
+++ plugins/gl_renderer/PCH.h
@@ -0,0 +1,24 @@
+#ifndef _PCH_H_
+#define _PCH_H_
+
+#include <vector>
+#include <list>
+#include <map>
+#include <xutility>
+
+using namespace std;
+
+#include "core/Core.h"
+
+using namespace hare_core;
+
+#include "graphics/Graphics.h"
+
+using namespace hare_graphics;
+
+#include "GLRenderPrerequisites.h"
+
+#include <GL/glx.h>
+#include <GL/gl.h>
+
+#endif

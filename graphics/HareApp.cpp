1bb3c0a39e3663282b760175c2ff58b91288a903
wanglei010407@hotmail.com <wanglei010407@hotmail.com@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-04-12 07:36:47 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@171 67360d68-41fe-4630-88ca-a5c15c9c3197

--- graphics/HareApp.cpp
+++ graphics/HareApp.cpp
@@ -25,14 +25,14 @@ namespace hare_graphics
 
 	void HareApp::startUp()
 	{
-		//DevILImageCodec::startUp();
+		DevILImageCodec::startUp();
 		textManager = new TextManager();
 	}
 
 	void HareApp::shutDown()
 	{
 		delete textManager;
-        //DevILImageCodec::shutDown();
+        DevILImageCodec::shutDown();
 	}
 
 	void HareApp::setGraphicsSystem(SystemManager* systemManager)

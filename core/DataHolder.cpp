9aec7cd2167b56a9de1a848d04095ea4d78ae700
wanglei010407@hotmail.com <wanglei010407@hotmail.com@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-05-24 11:19:16 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@373 67360d68-41fe-4630-88ca-a5c15c9c3197

---
+++ core/DataHolder.cpp
@@ -0,0 +1,58 @@
+//***************************************************************
+//  File:    DataHolder.cpp
+//  Date:    5/10/2009
+//  Author:  wanglei (wanglei010407@163.com)
+//-------------------------------------------------------------
+//
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+//
+//***************************************************************
+#include "PCH.h"
+#include "DataHolder.h"
+
+namespace hare
+{
+	DataHolder::DataHolder()
+		:data(NULL)
+		,size(0)
+	{
+
+	}
+
+	DataHolder::~DataHolder()
+	{
+		clear();
+	}
+
+	void DataHolder::set(uint8* data, uint32 size)
+	{
+		assert(data && size >  0);
+
+		clear();
+
+		this->data = data;
+		this->size = size;
+
+	}
+
+	void DataHolder::allocate(uint32 size)
+	{
+		assert(size);
+
+		clear();
+
+		this->data = new uint8[size];
+		this->size = size;
+		memset(this->data, 0, this->size);
+	}
+
+	void DataHolder::clear()
+	{
+		delete data;
+		data = 0;
+		size = 0;
+	}
+}

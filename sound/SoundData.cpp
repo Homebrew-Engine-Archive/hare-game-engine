e2dc3e8440d2ab29095edc0fff9a163d8cddcc6f
wanglei010407@hotmail.com <wanglei010407@hotmail.com@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-05-27 12:01:58 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@399 67360d68-41fe-4630-88ca-a5c15c9c3197

---
+++ sound/SoundData.cpp
@@ -0,0 +1,58 @@
+//***************************************************************
+//  File:    SoundData.cpp
+//  Date:    5/26/2009
+//  Author:  wanglei (wanglei010407@163.com)
+//-------------------------------------------------------------
+//
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+//
+//***************************************************************
+#include "PCH.h"
+#include "SoundData.h"
+#include "SoundApp.h"
+#include "SoundManager.h"
+
+namespace hare
+{
+
+    HARE_IMPLEMENT_DYNAMIC_CLASS(SoundData, Object, 0)
+    {
+        HARE_OBJ_F(soundParam, SoundParam, propAvoidNull);
+    }
+    
+    SoundData::SoundData()
+    {
+        soundBuffer = getSoundApp()->getSoundManager()->createSoundBuffer();
+    }
+
+    SoundData::~SoundData()
+    {
+
+    }
+
+    void SoundData::setSoundParam(SoundParam* param)
+    {
+        soundParam = param;
+    }
+
+    void SoundData::postEdited(Attribute *attr)
+    {
+        postLoaded();
+    }
+
+    void SoundData::postLoaded()
+    {   
+        soundBuffer->setSoundParam(soundParam);
+        soundBuffer->unload();
+        soundBuffer->load();
+        soundBuffer->update();
+    }
+
+    SoundBuffer* SoundData::getSoundBuffer()
+    {
+        return soundBuffer;
+    }
+}
---
+++ sound/SoundParam.cpp
@@ -0,0 +1,65 @@
+//***************************************************************
+//  File:    SoundData.cpp
+//  Date:    5/26/2009
+//  Author:  wanglei (wanglei010407@163.com)
+//-------------------------------------------------------------
+//
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+//
+//***************************************************************
+#include "PCH.h"
+#include "SoundParam.h"
+
+namespace hare
+{
+    HARE_IMPLEMENT_DYNAMIC_CLASS(SoundParam, Object, 0)
+    {
+        HARE_META(fileName, String)
+        HARE_META(innerCone, float)
+        HARE_META(outerCone, float)
+        HARE_META(minGain, float)
+        HARE_META(maxGain, float)
+        HARE_META(refDistance, float)
+        HARE_META(maxDistance, float)
+        HARE_META(pitch, float)
+        HARE_META(gain, float)
+        HARE_META(outerGain, float)
+        HARE_META(rolloff, float)
+        HARE_META(bFromStream, bool)
+    }
+
+
+    SoundParam::SoundParam()
+        :innerCone(360.0f)
+        ,outerCone(360.0f)
+        ,minGain(0.0f)
+        ,maxGain(1.0f)
+        ,refDistance(100.0f)
+        ,maxDistance(1000.0f)
+        ,pitch(1.0f)
+        ,gain(1.0f)
+        ,outerGain(0.0f)
+        ,rolloff(1.0f)
+        ,bFromStream(false)
+    {
+
+    }
+
+    SoundParam::~SoundParam()
+    {
+
+    }
+
+    void SoundParam::postEdited(Attribute *attr)
+    {
+
+    }
+
+    void SoundParam::postLoaded()
+    {
+
+    }
+}

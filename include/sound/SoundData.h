1f73c79dbfaa94869e29798f2678c7357db0676f
wanglei010407@hotmail.com <wanglei010407@hotmail.com@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-05-27 12:02:31 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@400 67360d68-41fe-4630-88ca-a5c15c9c3197

---
+++ include/sound/SoundData.h
@@ -0,0 +1,47 @@
+//***************************************************************
+//  File:    SoundData.h
+//  Date:    5/26/2009
+//  Author:  wanglei (wanglei010407@163.com)
+//-------------------------------------------------------------
+//
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+//
+//***************************************************************
+#ifndef _SOUNDDATA_H_
+#define _SOUNDDATA_H_
+
+#include "SoundPrerequisites.h"
+#include "SoundParam.h"
+#include "SoundBuffer.h"
+
+namespace hare
+{
+
+
+    class SOUND_API SoundData : public Object
+    {
+        HARE_DECLARE_DYNAMIC_CLASS(SoundData)
+    public:
+        SoundData();
+        virtual ~SoundData();
+
+        void setSoundParam(SoundParam* param);
+
+        SoundBuffer* getSoundBuffer();
+
+        virtual void postEdited(Attribute *attr);
+
+        virtual void postLoaded();
+
+    protected:
+        SoundParam::Ptr soundParam;
+
+        SoundBuffer::Ptr soundBuffer;
+
+    };
+}
+
+#endif
---
+++ include/sound/SoundParam.h
@@ -0,0 +1,51 @@
+//***************************************************************
+//  File:    SoundParam.h
+//  Date:    5/26/2009
+//  Author:  wanglei (wanglei010407@163.com)
+//-------------------------------------------------------------
+//
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+//
+//***************************************************************
+#ifndef _SOUNDPARAM_H_
+#define _SOUNDPARAM_H_
+
+#include "SoundPrerequisites.h"
+
+
+namespace hare
+{
+    class SOUND_API SoundParam : public Object
+    {
+        HARE_DECLARE_DYNAMIC_CLASS(SoundParam)
+    public:
+        SoundParam();
+        virtual ~SoundParam();
+
+        String  fileName;
+
+        float   innerCone;
+        float   outerCone;
+        float   minGain;
+        float   maxGain;
+        float   refDistance;
+        float   maxDistance;
+        float   pitch;
+        float   gain;
+        float   outerGain;
+        float   rolloff;
+        bool    bFromStream;
+
+        virtual void postEdited(Attribute *attr);
+
+        virtual void postLoaded();
+    };
+}
+
+
+#endif
+
+

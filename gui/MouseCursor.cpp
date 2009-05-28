83407579b8a3a45ce0ba691341126d4251a61b32
littlesome@live.cn <littlesome@live.cn@67360d68-41fe-4630-88ca-a5c15c9c3197>
2009-05-28 08:32:59 +0000

    git-svn-id: file:///mnt/data/Workspace/zie87/game_development/tmp/hare-game-engine/p/trunk@403 67360d68-41fe-4630-88ca-a5c15c9c3197

---
+++ gui/MouseCursor.cpp
@@ -0,0 +1,19 @@
+//***************************************************************
+//  File:    MouseCursor.cpp
+//  Data:    05/28/2009
+//  Author:  littlesome (littlesome@live.cn)
+//-------------------------------------------------------------
+//  
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+// 
+//***************************************************************
+#include "PCH.h"
+#include "MouseCursor.h"
+
+namespace hare
+{
+    HARE_IMPLEMENT_SINGLETON(MouseCursor)
+}
---
+++ include/gui/MouseCursor.h
@@ -0,0 +1,77 @@
+//***************************************************************
+//  File:    MouseCursor.h
+//  Data:    05/27/2009
+//  Author:  littlesome (littlesome@live.cn)
+//-------------------------------------------------------------
+//  
+//-------------------------------------------------------------
+//  This file is part of Hare2D Game Engine.
+//  Copyright (C) All Rights Reserved
+//***************************************************************
+// 
+//***************************************************************
+#ifndef _MOUSECURSOR_H_
+#define _MOUSECURSOR_H_
+
+#include "UIPrerequisites.h"
+
+namespace hare
+{
+    enum CursorState
+    {
+        Cursor_Arrow = 0,
+        Cursor_Moving,
+        Cursor_SizingNESW,
+        Cursor_SizingNS,
+        Cursor_SizingNWSE,
+        Cursor_SizingWE,
+
+        Cursor_MAX,
+
+        Cursor_None = -1,
+    };
+
+    class UI_API MouseCursor : public Singleton<MouseCursor>
+    {
+        HARE_DECLARE_SINGLETON(MouseCursor)
+    public:
+        MouseCursor() : currState(Cursor_None) {}
+
+        void setCursor(CursorState state)
+        {
+            currState = state;
+        }
+        
+        CursorState getCursor() const
+        {
+            return currState;
+        }
+        
+        void setCursorSprite(CursorState state, Sprite* sprite)
+        {
+            if (state == Cursor_None)
+                return;
+
+            cursors[state] = sprite;
+        }
+        
+        Sprite* getCursorSprite(CursorState state)
+        {
+            return cursors[state];
+        }
+
+        Sprite* getCursorSprite()
+        {
+            if (currState == Cursor_None)
+                return NULL;
+
+            return cursors[currState];
+        }
+
+    protected:
+        Sprite::Ptr cursors[Cursor_MAX];
+        CursorState currState;
+    };
+}
+
+#endif
